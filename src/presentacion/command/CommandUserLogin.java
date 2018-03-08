02a564db9a43ee6a46521cb65cb5b7f7b8b72b26
RKJMTW <zhong@ucm.es>
2018-03-08 10:23:55 +0100

    Fixed minor errors

--- src/presentacion/command/CommandUserLogin.java
+++ src/presentacion/command/CommandUserLogin.java
@@ -4,14 +4,14 @@ import negocio.FactoriaNegocio;
 import negocio.usuario.Usuario;
 import presentacion.Command;
 import presentacion.Contexto;
+import presentacion.Events;
 
 public class CommandUserLogin implements Command {
 
 	@Override
 	public Contexto execute(Object data) {
-		
-		return FactoriaNegocio.getInstance().generateSAUsuario.readByName(((Usuario)data).getNombre());
-	
+		Contexto context = new Contexto(Events.USER_LOGIN_OK, FactoriaNegocio.getInstance().generateSAUsuario().readByName(((Usuario)data).getNombre()));
+		return context;
 	}
 
 }
