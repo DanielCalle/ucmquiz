0b91d89a05e4919704dcc5fd0f90d4c7b5c77086
charliee97 <carlosgcereceda@hotmail.es>
2018-03-26 19:20:38 +0200

    Ventana de Seleccion de Usuario
    Se termina la tarea 1 de seleccion de usuario

---
+++ src/main/java/presentacion/Main.java
@@ -0,0 +1,33 @@
+package presentacion;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/presentacion/SeleccionUsuario.fxml"));
+		stage.setTitle("UCM QUIZ");
+		
+		//stage.setResizable(false);
+		stage.setScene(new Scene(root));
+		stage.show();
+		/*stage.setScene(new Scene(root2));
+		stage.show();*/
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/presentacion/guiControlers/SeleccionUsuarioController.java
@@ -0,0 +1,43 @@
+package presentacion.guiControlers;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SeleccionUsuarioController {
+
+	@FXML
+    private JFXButton btnprofesor;
+
+    @FXML
+    private JFXButton btnAdministrador;
+
+    @FXML
+    private JFXButton btnAlumno;
+
+    @FXML
+    void btnAdministradorListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAlumnoListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnprofesorListener(ActionEvent event) {
+    	
+    	
+    }
+
+
+
+}
