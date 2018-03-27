93debcaf926c9fa654a30b5fbeccb1d8e39c66a6
RKJMTW <zhong@ucm.es>
2018-03-27 16:18:17 +0200

    Entity Asignatura done

--- src/main/java/negocio/asignatura/Asignatura.java
+++ src/main/java/negocio/asignatura/Asignatura.java
@@ -1,8 +1,59 @@
 package negocio.asignatura;
 
-import javax.persistence.Entity;
+import java.util.Collection;
+
+import javax.persistence.*;
+
+import negocio.pregunta.Pregunta;
 
 @Entity
 public class Asignatura {
 
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Version
+	private int version;
+	
+	public Asignatura() {
+		
+	}
+	
+	public Asignatura(String titulo, boolean activo) {
+		this.setTitulo(titulo);
+		this.setActivo(activo);
+	}
+	
+	@OneToMany(mappedBy="asignatura")
+	private Collection<Pregunta> preguntas;
+
+	private String titulo;
+
+	private boolean activo;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
 }
