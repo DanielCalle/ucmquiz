7781eba13c92cfb34bf1fc0ec1a2e373a9281857
Josué Pradas <jpradas@ucm.es>
2018-03-28 23:01:21 +0200

    Creado test para borrar pregunta como profesor

--- src/test/java/negocio/pregunta/SAPreguntaTest.java
+++ src/test/java/negocio/pregunta/SAPreguntaTest.java
@@ -1,5 +1,34 @@
 package negocio.pregunta;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.junit.Test;
+
+import negocio.EntityManagerUtil;
+
 public class SAPreguntaTest {
 
+	@Test
+	public void borrar_pregunta_deberia_dejar_su_asignatura_en_null(int idPregunta) {
+		Pregunta p = null;
+		EntityManager entitymanager = EntityManagerUtil.getEntityManager();
+		EntityTransaction entitytransaction = entitymanager.getTransaction();
+		entitytransaction.begin();
+		
+		try {
+			p = entitymanager.find(Pregunta.class, idPregunta); //se busca el objeto por la clave primaria (el id)
+		
+			assertEquals(p.getAsignatura(), null);
+			
+		}catch(IllegalArgumentException iae) { //si el id pasado no existe el entitymanager soltará error
+			iae.printStackTrace();
+			entitytransaction.rollback();
+		}finally {
+			entitymanager.close();
+		}
+	}
+	
 }
