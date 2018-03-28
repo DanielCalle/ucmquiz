bcdb6a98b421f790d958439bb43e99a1dc14ddcb
charliee97 <carlosgcereceda@hotmail.es>
2018-03-28 17:55:55 +0200

    Controller de CrearAsignatura

---
+++ src/main/java/presentacion/guiControlers/CrearAsignaturaController.java
@@ -0,0 +1,35 @@
+package presentacion.guiControlers;
+
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CrearAsignaturaController {
+    @FXML
+    private JFXButton btncancelar;
+
+    @FXML
+    private JFXTextField textfieldAsignatura;
+
+    @FXML
+    private JFXButton btncrear;
+
+    @FXML
+    void btncancelarListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btncrearListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfieldAsignaturaListener(ActionEvent event) {
+
+    }
+}
+
