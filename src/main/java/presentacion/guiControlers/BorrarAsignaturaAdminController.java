ca974bbd99e3511c4b45e56bff1f5a742f49ee39
Ana Corral <analauracorral@gmail.com>
2018-03-29 22:08:14 +0200

    Pantalla borrar asignatura como administrador -> fxml y controller

---
+++ src/main/java/presentacion/guiControlers/BorrarAsignaturaAdminController.java
@@ -0,0 +1,35 @@
+package presentacion.guiControlers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class BorrarAsignaturaAdminController {
+
+    @FXML
+    private JFXButton btncancelar;
+
+    @FXML
+    private JFXTextField textfieldAsignatura;
+
+    @FXML
+    private JFXButton btnborrar;
+
+    @FXML
+    void btnBorrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textFieldAsignatura(ActionEvent event) {
+
+    }
+
+}
