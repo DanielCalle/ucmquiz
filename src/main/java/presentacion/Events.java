3afe0fe657be0e6cb69da50d16b2aab7dbead77f
flynn <flynn@LaRed>
2018-04-03 18:21:44 +0200

    T51

--- src/main/java/presentacion/Events.java
+++ src/main/java/presentacion/Events.java
@@ -1,5 +1,61 @@
 package presentacion;
 
-public class Events {
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+
+public enum Events {
+	
+	CRUD_CREATE_OK,
+	CRUD_CREATE_KO,
+	CRUD_READ_OK,
+	CRUD_READ_KO,
+	CRUD_READ_ALL_OK,
+	CRUD_READ_ALL_KO,
+	CRUD_UPDATE_OK,
+	CRUD_UPDATE_KO,
+	CRUD_DELETE_OK,
+	CRUD_DELETE_KO,
+	
+	SHOW_LOGIN,
+	SHOW_ROL_MENU,
+	
+	COMMAND_USER_LOGIN,
+	
+	;
+
+	private InputSource inputSource;
+	
+	private Events() {
+		this.inputSource = new InputSource("Events.xml");
+	}
+	
+	public String getMessage() {
+
+		String message = null;
+		
+		try {
+		
+			XPathFactory xPathFactory = XPathFactory.newInstance();
+			
+			XPath xpath = xPathFactory.newXPath();
+	        
+			Node element = (Node) xpath.evaluate("//*[@id='"+ this.name() +"'][1]",inputSource,XPathConstants.NODE);
+			
+			message = element.getTextContent().trim();
+			
+		} catch (XPathExpressionException e) {
+			
+			e.printStackTrace();
+		
+		}
+		
+		return message;
+		
+	}
 	
 }
