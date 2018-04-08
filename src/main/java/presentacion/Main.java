5d990573c33643e4d96d578f15bf7b1cb720a5c9
charliee97 <carlosgcereceda@hotmail.es>
2018-04-08 19:03:12 +0200

    Menu

--- src/main/java/presentacion/Main.java
+++ src/main/java/presentacion/Main.java
@@ -3,9 +3,6 @@ package presentacion;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import presentacion.controlador.Controlador;
 
