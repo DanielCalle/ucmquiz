61aa7e90f7db822f0792129cc6491c062191a17d
IGNACIO <IGNACIO@DESKTOP-EH1DOIU>
2018-04-08 12:56:41 +0200

    Añadido filtro

--- src/main/java/negocio/asignatura/SAAsignaturaImp.java
+++ src/main/java/negocio/asignatura/SAAsignaturaImp.java
@@ -10,12 +10,16 @@ import javax.persistence.TypedQuery;
 import negocio.EntityManagerUtil;
 import presentacion.Contexto;
 import presentacion.Events;
+import presentacion.Filter;
 
 public class SAAsignaturaImp implements SAAsignatura {
 
 	@Override
 	public Contexto readAll() {
 		List<Asignatura> lista = null;
+		Events event = null;
+		Filter filter = new Filter();
+		Contexto contexto = null;
 		try {
 			EntityManager entitymanager = EntityManagerUtil.getEntityManager();
 			EntityTransaction entitytransaction = entitymanager.getTransaction();
@@ -23,14 +27,34 @@ public class SAAsignaturaImp implements SAAsignatura {
 				
 			TypedQuery<Asignatura> query = entitymanager.createNamedQuery("negocio.asignatura.Asignatura.readAll", Asignatura.class);
 			lista = query.getResultList();
-			entitytransaction.commit();
+			if (lista.isEmpty()) {
+				entitytransaction.rollback();
 				
+				event = Events.CRUD_READ_ALL_KO;
+				
+				filter.addFilter("reason","no existen asignaturas");
+				
+				filter.addFilter("info", "");
+				
+				event.setFilter(filter);
+				
+				contexto = new Contexto(event,lista);
+			}
+			else {
+			entitytransaction.commit();
+			event = Events.CRUD_READ_ALL_OK;
+			
+			filter.addFilter("info", "");
+			
+			event.setFilter(filter);
+			
+			contexto = new Contexto(event,lista);
+			} 
 			entitymanager.close();
-		} catch(PersistenceException ex) {
 		}
-		if (lista.equals(null))
-			return new Contexto(Events.CRUD_READ_ALL_KO, null);
-		return new Contexto(Events.CRUD_READ_ALL_OK, lista);
+		catch(PersistenceException ex) {
+		}
+	return contexto;	
 	}
 
 }
