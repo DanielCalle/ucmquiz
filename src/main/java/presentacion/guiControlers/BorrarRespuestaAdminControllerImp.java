631a13c59bc247977ea6c4eb8960719abd2d7b01
Ana Corral <analauracorral@gmail.com>
2018-05-04 12:12:30 +0200

    Correcion pequeños fallos

--- src/main/java/presentacion/guiControlers/BorrarRespuestaAdminControllerImp.java
+++ src/main/java/presentacion/guiControlers/BorrarRespuestaAdminControllerImp.java
@@ -2,20 +2,25 @@ package presentacion.guiControlers;
 
 import presentacion.Contexto;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXComboBox;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminController{
+	
+	@FXML
+    private AnchorPane root;
+	
 	@FXML
 	private JFXButton btncancelar;
 
 	@FXML
-	private JFXTextField textfieldAsignatura;
-
-	@FXML
 	private JFXButton btnborrar;
+	
+	@FXML
+	private JFXComboBox<?> comboboxRepuesta;
 
 	@FXML
 	void btnBorrar(ActionEvent event) {
