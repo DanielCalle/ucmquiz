e5618b59f6bf9971435cac797d297372360d698f
Ana Corral <analauracorral@gmail.com>
2018-05-04 11:54:05 +0200

    Creada vista para borrar pregunta como administrador (controller y fxml)

---
+++ src/main/java/presentacion/guiControlers/BorrarRespuestaAdminControllerImp.java
@@ -0,0 +1,37 @@
+package presentacion.guiControlers;
+
+import presentacion.Contexto;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminController{
+	@FXML
+	private JFXButton btncancelar;
+
+	@FXML
+	private JFXTextField textfieldAsignatura;
+
+	@FXML
+	private JFXButton btnborrar;
+
+	@FXML
+	void btnBorrar(ActionEvent event) {
+
+	}
+
+	@FXML
+	void btnCancelar(ActionEvent event) {
+
+	}
+
+	@Override
+	public void update(Contexto contexto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
