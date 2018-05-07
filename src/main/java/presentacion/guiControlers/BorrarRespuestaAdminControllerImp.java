8c82eaf6b1f290ed7bfcd1e507a6d48647a7c88d
ivs95 <ivs199522@gmail.com>
2018-05-07 15:14:34 +0200

    Arreglos HU38

--- src/main/java/presentacion/guiControlers/BorrarRespuestaAdminControllerImp.java
+++ src/main/java/presentacion/guiControlers/BorrarRespuestaAdminControllerImp.java
@@ -1,8 +1,10 @@
 package presentacion.guiControlers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
@@ -13,6 +15,7 @@ import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.controls.RecursiveTreeItem;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -26,6 +29,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import negocio.asignatura.Asignatura;
 import negocio.pregunta.Pregunta;
 import negocio.respuesta.Respuesta;
 import presentacion.Contexto;
@@ -47,16 +51,21 @@ public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminContr
 	private JFXTreeTableView<Respuesta> tablaRespuestas;
 
 	@FXML
-	private JFXComboBox<?> comboboxPregunta;
+	private JFXComboBox<String> comboboxPregunta;
+	
+	private List<Respuesta> listaRespuestas;
+	private List<Pregunta> listaPreguntas;
 
 	@FXML
 	void btnBorrar(ActionEvent event) {
 		if (comboboxPregunta.getSelectionModel().getSelectedItem() != null) {
+				Contexto contexto = new Contexto(Events.CRUD_READ_PREGUNTA_OK, listaPreguntas.get(comboboxPregunta.getSelectionModel().getSelectedIndex()));
+				this.update(contexto);
 			if (tablaRespuestas.getSelectionModel().getSelectedItem() != null) {
 				TreeItem<Respuesta> resp = tablaRespuestas.getSelectionModel().getSelectedItem();
 				Respuesta r = resp.getValue();
-				Contexto contexto = new Contexto(Events.COMMAND_RESPUESTA_DELETE_ADMIN, r.getTexto());
-				Controlador.getInstance().accion(contexto);
+				Contexto contexto3 = new Contexto(Events.COMMAND_RESPUESTA_DELETE_ADMIN, r.getTexto());
+				Controlador.getInstance().accion(contexto3);
 			}
 			else {
 				JFXDialogLayout content = new JFXDialogLayout();
@@ -77,8 +86,7 @@ public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminContr
 				content.setActions(button);
 				dialog.show();
 			}
-		}
-		else {
+		}else {
 			JFXDialogLayout content = new JFXDialogLayout();
 			content.setHeading(new Text("Accion incorrecta"));
 			content.setBody(new Text("Debes seleccionar una pregunta"));
@@ -153,10 +161,10 @@ public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminContr
 			break;
 		case CRUD_READ_ALL_RESPUESTA_OK:
 
-			List<Respuesta> lista = (List<Respuesta>) contexto.getDato();
+			this.listaRespuestas = (List<Respuesta>) contexto.getDato();
 			ObservableList<Respuesta> users = FXCollections.observableArrayList();
 
-			for (Respuesta r : lista) {
+			for (Respuesta r : listaRespuestas) {
 				users.add(r);
 			}
 			final TreeItem<Respuesta> raiz = new RecursiveTreeItem<Respuesta>(users, RecursiveTreeObject::getChildren);
@@ -184,6 +192,34 @@ public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminContr
 			contenido.setActions(button1);
 			dialogo.show();
 			break;
+			
+		case CRUD_READ_PREGUNTA_OK:
+			// Recibe la asignatura lo pone como titulo y llama al read all
+			Pregunta preguntaActual = (Pregunta) contexto.getDato();
+			List<Respuesta> respuestas = preguntaActual.getRespuestas();
+			ObservableList<Respuesta> respuestasList = FXCollections.observableArrayList();
+			for (Respuesta r : respuestas) {
+				respuestasList.add(r);
+			}
+			final TreeItem<Respuesta> raiz2 = new RecursiveTreeItem<Respuesta>(respuestasList, RecursiveTreeObject::getChildren);
+			tablaRespuestas.setRoot(raiz2);     
+			break;
+				      	
+		case CRUD_READ_ALL_PREGUNTA_KO:
+			JFXDialogLayout contenido2 = new JFXDialogLayout();
+			contenido2.setHeading(new Text("Error al leer las respuestas"));
+			contenido2.setBody(new Text(contexto.getEvent().getMessage()));
+			JFXDialog dialogo2 = new JFXDialog(root, contenido2, JFXDialog.DialogTransition.CENTER);
+			JFXButton button2 = new JFXButton("Ok");
+			button2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+				public void handle(ActionEvent arg0) {
+					dialogo2.close();
+				}
+			});
+			contenido2.setActions(button2);
+			dialogo2.show();
+			break;
 
 		default:
 			
@@ -209,25 +245,12 @@ public class BorrarRespuestaAdminControllerImp extends BorrarRespuestaAdminContr
 	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		JFXTreeTableColumn<Respuesta, String> respuesta = new JFXTreeTableColumn<>("Respuestas");
-		respuesta.setPrefWidth(630);
-		respuesta.setCellValueFactory(
-				new Callback<TreeTableColumn.CellDataFeatures<Respuesta, String>, ObservableValue<String>>() {
-					@Override
-					public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Respuesta, String> param) {
-						return param.getValue().getValue().getTituloStringProperty();
-					}
-				});
-		ObservableList<Pregunta> users = FXCollections.observableArrayList();
-		tablaRespuestas.getColumns().setAll(respuesta);
-		tablaRespuestas.setShowRoot(false);
-		tablaRespuestas.getSelectionModel().getSelectedItem();
-
-		Contexto contexto = new Contexto(Events.COMMAND_RESPUESTA_READ_ALL, null);
+		Contexto contexto = new Contexto(Events.COMMAND_PREGUNTA_READ_ALL, null);
 		Controlador.getInstance().accion(contexto);
 
+		List<String> value = listaPreguntas.stream().map(a -> a.getTexto()).collect(Collectors.toList());
+		comboboxPregunta.setItems(FXCollections.observableArrayList(value));
 		
 	}
-
 
 }
